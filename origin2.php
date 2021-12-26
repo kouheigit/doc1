3bc96378c7bf8d1ed4f920f57fb8193fea803ffe
ookawa <ookawakouhei777@gmail.com>
2021-12-26 21:36:33 +0900

    second commit

---
+++ origin2.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+ <html>
+  <head>
+   <meta charset="UTF8-8">
+   <meta name="viewport" content="width=device-width,initial-scale=1">
+   <meta name="description" content="ページの内容を説明する">
+   <link rel="stylesheet" href="css/origin2.css">
+   <script src="JavaScriptファイルのパス"></script>
+ </head>
+ <body>
+  <section>
+   <h2>シニアフォン　３つのメリット</h2>
+   <div class="itembox">
+    <div class="item">
+      <dt>使い方が簡単</dt>
+       <br>
+      <dd>大画面のタッチパネルで操作が楽々、機能が絞られてるので迷う事はありません。</dd>
+     </div>
+      <div class="item">
+       <dt>毎月定額で安心</dt>
+         <br>
+       <dd>「通信料ってよくわからない....知らないうちに高くなったりしない?」などの心配も無用です。</dd>
+     </div>
+       <div class="item">
+	<dt>充実のサポート</dt>
+         <br>
+        <dd>コールセンターだけではなく訪問サービスもございます。</dd>
+    </div>
+   </div>
+  </section>
+ </body>
+</html>
